462ed20e3b09e157c65e2542434b33e6e69f5d70
mdeverdelhan <marc.deverdelhan@yahoo.com>
2015-03-26 09:10:31 +0100

    Fix printf format

--- src/main/java/eu/verdelhan/acr122urw/MifareUtils.java
+++ src/main/java/eu/verdelhan/acr122urw/MifareUtils.java
@@ -225,7 +225,7 @@ public final class MifareUtils {
      * @param keys the keys to be tested for reading
      */
     private static void dumpMifareClassic1KBlock(MfReaderWriter reader, MfCard card, int sectorId, int blockId, List<String> keys) throws CardException {
-        System.out.printf("Sector %02%d block %02%d: ", sectorId, blockId);
+        System.out.printf("Sector %02d block %02d: ", sectorId, blockId);
         for (String key : keys) {
             // For each provided key...
             if (isValidMifareClassic1KKey(key)) {
